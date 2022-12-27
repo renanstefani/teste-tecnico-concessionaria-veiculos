85bff3a5391d139ba95f5e6c961d3acc79ef4ef1
Renan Stefani <renanstefanir@gmail.com>
2022-12-27 00:58:59 -0300

    Inserindo método DELETE
    Inserindo script utilizado para remover um veículo do DB

--- APIveiculos.php
+++ APIveiculos.php
@@ -20,6 +20,11 @@ switch ($request_method) {
         // Inserir veículo
         inserir_veiculo();
         break;
+    case 'DELETE':
+        // Remover veículo
+        $id = intval($_GET["id"]);
+        remover_veiculo($id);
+        break;
     default:
         // Invalid Request Method
         header("Método de requisição inválido");
@@ -86,5 +91,28 @@ function inserir_veiculo()
     echo json_encode($response);
 }
 
+// Removendo veículo do banco de dados
+function remover_veiculo($id)
+{
+    global $connection;
+
+    // executando a requisição
+    $query = "DELETE FROM veiculos WHERE id=" . $id;
+
+    if (mysqli_query($connection, $query)) {
+        // caso tenha um id especificado o veículo será removido do DB
+        $response = array(
+            'status' => 1,
+            'status_message' => 'Veículo removido com sucesso.'
+        );
+    } else {
+        $response = array(
+            'status' => 0,
+            'status_message' => 'Falha ao tentar remover o veículo.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
 
 ?>
